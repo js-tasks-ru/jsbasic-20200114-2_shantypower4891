d65d1095c9eae0c7249a61e2c13cb15921ec947c
shantypower1984 <a.n@alpham.pro>
2020-01-24 14:09:14 +0900

    добавляет задачу factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,13 @@
  * @returns {number}
  */
 function factorial(n) {
-
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    do {
+      n = n * (n - 1) ;
+    }
+    while (n > 1);
+    return console.log(n + '! = ', factorial(n));
+  }
 }
