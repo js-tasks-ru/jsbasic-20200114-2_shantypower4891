3f39df448a0e6df5d5ac3d00b09722a75953aea9
shantypower1984 <a.n@alpham.pro>
2020-02-15 17:34:45 +0900

    решает задачу  makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let list = document.createElement('ul');
+  let listItem = '';
+  for (let friend of friends) {
+    listItem += `<li>${friend.firstName} ${friend.lastName}</li>`;
+  }
+  list.innerHTML = listItem;
+  return list;
 }
+
