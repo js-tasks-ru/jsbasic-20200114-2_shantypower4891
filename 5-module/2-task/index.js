3c731526dc387443575511a9e35af4a43b9f1e57
shantypower1984 <a.n@alpham.pro>
2020-02-21 17:14:13 +0900

    решает задачу SortableTable

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -13,11 +13,38 @@
  *
  * @constructor
  */
-function SortableTable(items) {
+class SortableTable {
   /**
    * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
    */
-  this.el = document.createElement('table');
+  constructor(items) {
+    this.el = document.createElement('table');
+    this.items = items;
+    const thead = '<thead><tr><td>Name</td><td>Age</td><td>Salary</td><td>City</td></tr></thead>';
+    const tbody = '<tbody></tbody>';
+    this.el.insertAdjacentHTML('afterbegin', thead);
+    this.el.insertAdjacentHTML('beforeend', tbody);
+    this.colProperty = ['name', 'age', 'salary', 'city'];
+    this.render();
+  }
+
+
+  render(desc) {
+    const body = this.el.querySelector('tbody');
+    body.innerHTML = '';
+    const place = desc ? 'afterbegin' : 'beforeend';
+
+    for (let person of this.items) {
+      let name = `<td>${person.name}</td>`;
+      let age = `<td>${person.age}</td>`;
+      let salary = `<td>${person.salary}</td>`;
+      let city = `<td>${person.city}</td>`;
+      let tr = name + age + salary + city;
+
+      body.insertAdjacentHTML(place, tr);
+
+    }
+  }
 
   /**
    * Метод выполняет сортировку таблицы
@@ -25,5 +52,26 @@ function SortableTable(items) {
    * нужно выполнить сортировку (отсчет начинается от 0)
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
-  this.sort = (column, desc = false) => {};
+
+  sort(column, desc = false) {
+
+    this.chosenColumn = column;
+
+    this.items.sort((a, b) => {
+      const index = this.colProperty[this.chosenColumn];
+      let result = a[index] - b[index];
+      if (!isNaN(result))
+        return result;
+
+      if (a[index] > b[index])
+        return 1;
+
+      if (a[index] == b[index])
+        return 0;
+
+      return -1;
+    });
+
+    this.render(desc);
+  }
 }
