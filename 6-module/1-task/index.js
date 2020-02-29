22c04a3e7099408c9f9fdecdf3dea731dd3363d2
shantypower1984 <a.n@alpham.pro>
2020-02-29 16:37:56 +0900

    решает задачу ClearedTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -15,20 +15,18 @@
  */
 class ClearedTable {
   constructor(data) {
-    this.el = document.createElement('table');
+    this.el = document.createElement("table");
     this.data = data;
-    const thead = '<thead><tr><td>Name</td><td>Age</td><td>Salary</td><td>City</td></tr></thead>';
-    const tbody = '<tbody></tbody>';
-    this.el.insertAdjacentHTML('afterbegin', thead);
-    this.el.insertAdjacentHTML('beforeend', tbody);
-    this.colProperty = ['id', 'name', 'age', 'salary', 'city'];
+    const thead =
+      "<thead><tr><td>Name</td><td>Age</td><td>Salary</td><td>City</td></tr></thead>";
+    const tbody = "<tbody></tbody>";
+    this.el.insertAdjacentHTML("afterbegin", thead);
+    this.el.insertAdjacentHTML("beforeend", tbody);
+    this.colProperty = ["id", "name", "age", "salary", "city"];
     const id = this.colProperty[0];
-    this.render(id);
-  }
-
-  render(id) {
-    const body = this.el.querySelector('tbody');
-    body.innerHTML = '';
+    //this.render(id);
+    const body = this.el.querySelector("tbody");
+    //body.innerHTML = "";
     //const place = desc ? 'afterbegin' : 'beforeend';
 
     for (let person of this.data) {
@@ -39,23 +37,50 @@ class ClearedTable {
       let closebtn = `<td><a data-id="${person.id}" href="#delete">X</a></td>`;
       let tr = name + age + salary + city + closebtn;
       if (id != person.id) {
-        body.insertAdjacentHTML('beforeend', tr);
+        body.insertAdjacentHTML("beforeend", tr);
       }
     }
+    this.el.addEventListener("click", event => this.onClick(event));
   }
 
+  onClick(event) {
+    let btn = event.target.tagName;
+    if (btn != 'A') {
+      return;
+    }
+    let row = event.target.closest("tr");
+    event.preventDefault();
+    let id = row.querySelector("[data-id]").getAttribute("data-id");
+    row.remove();
 
-/**
- * Метод который вызывается после удалении строки
- * @param {number} id - идентификатор удаляемого пользователя
- */
-onRemoved(id) {
-  this.render(id);
-  console.log(`Из таблицы удален пользователь ${id}`);
+    this.onRemoved(parseInt(id, 10));
+  }
 
+/*   render(id) {
+    const body = this.el.querySelector("tbody");
+    body.innerHTML = "";
+    //const place = desc ? 'afterbegin' : 'beforeend';
+
+    for (let person of this.data) {
+      let name = `<td>${person.name}</td>`;
+      let age = `<td>${person.age}</td>`;
+      let salary = `<td>${person.salary}</td>`;
+      let city = `<td>${person.city}</td>`;
+      let closebtn = `<td><a data-id="${person.id}" href="#delete">X</a></td>`;
+      let tr = name + age + salary + city + closebtn;
+      if (id != person.id) {
+        body.insertAdjacentHTML("beforeend", tr);
+      }
+    }
+  } */
+
+  /**
+   * Метод который вызывается после удалении строки
+   * @param {number} id - идентификатор удаляемого пользователя
+   */
+  onRemoved(id) {
+    console.log(`Из таблицы удален пользователь ${id}`);
+  }
 }
-}
-
-
 
 window.ClearedTable = ClearedTable;
